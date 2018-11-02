65552eb7a0804f129fe7d1e19c77dba9fe6b0ac6
Anuj Panchal <anujpan1@gmail.com>
2018-11-02 19:23:09 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,12 @@
-def starts_with_a_vowel?(word)
+require 'pry'
 
+def starts_with_a_vowel?(word)
+  word.match(/\A[aeiouAEIOU]/) ? true : false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/\A[un]\z[ing]/)
+  binding.pry
 end
 
 def words_five_letters_long(text)
